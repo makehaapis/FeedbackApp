354bfdaee54d11cbe9f028898ff4e971ea0bf73d
Marko Haapasaari <marko1927@student.hamk.fi>
2024-01-06 17:36:11 +0200

    Depencies installed

--- feedbackapp.client/src/main.js
+++ feedbackapp.client/src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTwitter, faPhone, faInstagram);
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+const pinia = createPinia();
+
+createApp(App)
+    .use(router)
+    .use(pinia)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
